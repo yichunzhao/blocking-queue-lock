33864c609166ef707142ec9cf8e032539f612a41
yichunzhao <zhaoyichun@hotmail.com>
2020-10-19 22:54:06 +0200

    Consumer using AtomicInteger rather than sync methods.

--- src/main/java/com/ynz/multiplethreads/Consumer.java
+++ src/main/java/com/ynz/multiplethreads/Consumer.java
@@ -3,28 +3,21 @@ package com.ynz.multiplethreads;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 @Slf4j
 @RequiredArgsConstructor(staticName = "of")
 public class Consumer<T> implements Runnable {
-    private static int count = 0;
+    private static AtomicInteger count = new AtomicInteger();
 
     private final MyQueue<T> tMyQueue;
-
-    synchronized private static void increaseOne() {
-        count++;
-    }
-
-    synchronized private static int count() {
-        return count;
-    }
 
     @Override
     public void run() {
         while (true) {
             T item = tMyQueue.dequeue();
-            increaseOne();
 
-            log.info("consumed " + item + " and total count: " + count());
+            log.info("consumed " + item + " and total count: " + count.addAndGet(1));
 
             if (item.equals(Integer.MAX_VALUE)) {
                 log.info("consumer receiving an ending ");
